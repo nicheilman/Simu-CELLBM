43a674e8b172302a0a0e253bd8d026e22f07a919
nicheilman <heilman7@msu.edu>
2026-01-14 10:03:37 -0500

    RBC Setup

--- src/mesh/cell.cpp
+++ src/mesh/cell.cpp
@@ -1572,12 +1572,12 @@ void cell::periodicBCs() noexcept{
 
    //First get the centroid of the cell
     const vec3 centroid = get_centroid();
-if(get_cell_type_id() == 0){
-    if (centroid.dz() <= -0.75e-5){
-	const vec3 transvect(0., 0., 1.5e-5);
+if(get_cell_type_id() == 5){
+    if (centroid.dz() < -2.5e-5){
+	const vec3 transvect(0., 0., 5.0e-5);
         translate(transvect);	
-    } else if (centroid.dz() >= 0.75e-5){
-        const vec3 transvect(0., 0., -1.5e-5);
+    } else if (centroid.dz() >= 2.5e-5){
+        const vec3 transvect(0., 0., -5.0e-5);
         translate(transvect);
 		}
 
